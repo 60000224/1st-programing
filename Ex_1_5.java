07e8c9cc5c74862bc18b1610196c5efc7dbee60b
Aidan Hogan <60000224@ironschools.org>
2018-09-19 10:13:54 -0600

    create Ex_1_5

---
+++ Ex_1_5.java
@@ -0,0 +1,12 @@
+/*
+Author: Aidan Hogan
+Date: 9/19/18
+
+output result of equation
+*/
+
+class Ex_1_5 {
+	public static void main(String[] args) {
+		System.out.println( ( (9.5 * 4.5) - (2.5 * 3) ) / (45.5 - 3.5) );
+	}
+}
